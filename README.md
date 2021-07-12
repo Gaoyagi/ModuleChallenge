c6a2410f71d53f3966912b840a0cb4f9c29ef417
Gaoyagi <aoyagigeorge@gmail.com>
2021-07-12 11:09:12 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+https://img.shields.io/github/last-commit/gaoyagi/ModuleChallenge
+
+# takes in user inputted string and returns the word count for it
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 
 exports.msgLen = function() {
-    const message = prompt("Input a String");
+    const message = prompt("Input a String: ");
     words = String(message).split(" ")
     console.log("this message has " + words.length + " words")
   }
