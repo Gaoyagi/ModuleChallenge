1784f773486c324ccaeaf4e4a1e5056bc6c0ad40
Gaoyagi <aoyagigeorge@gmail.com>
2021-07-12 10:22:47 -0700

    changed to take in user input

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-exports.printMsg = function(message) {
+const prompt = require('prompt-sync')();
+
+exports.msgLen = function() {
+    const message = prompt("Input a String");
     words = String(message).split(" ")
     console.log("this message has " + words.length + " words")
   }
